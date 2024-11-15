61e92dcdde1d1e14591700cb065efe93258acc81
Donik Goxha <donik.godzha@students.finki.ukim.mk>
2024-11-15 11:47:19 +0100

    adding new or just working on existing... idk anymore

--- ispitni_vezhbi/ispitna23_kolokvium_mk_zad2_grupa2.cpp
+++ ispitni_vezhbi/ispitna23_kolokvium_mk_zad2_grupa2.cpp
@@ -3,7 +3,8 @@
 //
 //same as 21 with proizvod aka zad2 prv kolok lani
 /*
-input: 123456789
+input:
+123456789
 output:
 123456772
 123456714
@@ -15,18 +16,30 @@ output:
 1234518
 123458
 123440
-12344
-12316
-1236
-1218
-128
-116
-16
-6
+12340
+1230
+120
+10
+0
 */
 #include <iostream>
 using namespace std;
 
 int main() {
+    int broj;
+    while (cin >> broj) {
+        while (broj >= 10) {
+            int ld = broj % 10;
+            int sld = (broj / 10) % 10;
+            broj = broj / 100;
+            int proizvod = ld * sld;
+            if (proizvod < 10) {
+                broj = broj * 10 + proizvod;
+            } else {
+                broj = broj * 100 + proizvod;
+            }
+            cout << broj << endl;
+        }
+    }
     return 0;
 }
