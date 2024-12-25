6737229496ed55c97d9ce8ebbe1b98275aeaf6f6
Donik Goxha <donik.godzha@students.finki.ukim.mk>
2024-12-25 21:33:07 +0100

    adding new or just working on existing... idk anymore

--- aud_vezhbi/zad54.cpp
+++ aud_vezhbi/zad54.cpp
@@ -3,21 +3,42 @@
 #include <cstring>
 using namespace std;
 
-bool palindormeStr(char *txt, int left, int right) {
+bool palindrome(char *txt, int left, int right) {
     if (left >= right) {
         return true;
-    }
-    if (tolower(txt[left] != txt[right])) {
-        return false;
     } else {
-        return palindormeStr(txt, left + 1, right - 1);
+        if (tolower(txt[left]) != tolower(txt[right])) {
+            return false;
+        } else {
+            return palindrome(txt, left + 1, right - 1);
+        }
+    }
+}
+
+bool palindromeSentence(char *txt, int left, int right) {
+    if (left >= right) {
+        return true;
+    } else {
+        if (!isalpha(txt[left])) {
+            return palindromeSentence(txt, left + 1, right);
+        }
+
+        if (!isalpha(txt[right])) {
+            return palindromeSentence(txt, left, right - 1);
+        }
+
+        if (tolower(txt[left]) != tolower(txt[right])) {
+            return false;
+        } else {
+            return palindromeSentence(txt, left + 1, right - 1);
+        }
     }
 }
 
 int main() {
-    char txt1[101];
-    cin.getline(txt1, 101);
+    char txt[100];
+    cin.getline(txt, 100);
 
-    cout << palindormeStr(txt1, 0, strlen(txt1) - 1);
+    cout << palindromeSentence(txt, 0, strlen(txt) - 1);
     return 0;
 }
